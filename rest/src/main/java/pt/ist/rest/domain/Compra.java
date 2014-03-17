e0ad3c5445b0cdffea15059ea1714b5374c53cdf
PeBraz <pbraz.93>
2014-03-17 18:57:43 +0000

    changes to compra e item

--- rest/src/main/java/pt/ist/rest/domain/Compra.java
+++ rest/src/main/java/pt/ist/rest/domain/Compra.java
@@ -8,29 +8,46 @@ public class Compra extends Compra_Base {
         setConfirma(new Integer(0));
     }
     
-    public void adicionaItem(Prato novoItem){
+    public void adicionaItem(Prato prato, int quantidadePrato){
 	//cria um item novo, se nao existir, se existir actualiza a quantidade e destrói se for menor ou igual a 0
-/*
-		if (this.hasItem(item))
-			if (item.get)
-    	else
-			this.setCusto(mudaCusto(item));
-			*/super.addItem(item);
+		final Item item = getItemDoPrato();
+		if (item == null)
+			super.addItem(new Item(prato, quantidadePrato));
+		else
+			item.mudaQuantidade(quantidadePrato);
+		
+		if (item.getQuantidade() =< 0){
+			super.removeItem(item);
+			final int custoCompraSemItem = item.getQuantidade - quantidadePrato();	//custo deste prato na compra
+			this.mudaCusto(item,(-custoCompraSemItem));
+		}
+		else this.mudaCusto(item, quantidadePrato);
     }
 
     public void removeItem(Item item){
     	super.removeItem(item);	
     }
 
-    public void mudaCusto(Item item){
+    public void mudaCusto(Item item,int quantidadePrato){
         final int CUSTO = this.getCusto();
         final int PRECO = item.getPrato().getPreco();
-        final int QUANTIDADE = item.getQuantidade();
-        this.setCusto(CUSTO + (PRECO * QUANTIDADE));
+        this.setCusto(CUSTO + (PRECO * quantidadePrato));
     }
-
-
-
+	public boolean getItem(Prato prato){
+		for (Item i: getItemSet()){
+			if (i.isItemde(prato))
+				return i;
+		}
+		return null;
+	}
+	
+	public boolean compraTemPrato(Prato prato){
+		for (Item i: getItemSet()){
+			if (i.isItemDe(prato))
+				return true;
+		}
+		return false;
+	}
     
 
 }
