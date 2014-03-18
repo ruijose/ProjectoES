98f0d710dbe79e3e40c796e07a7b24e3831db0fa
Karna <karanbalu3@hotmail.com>
2014-03-18 12:01:07 +0000

    dois servicos de listagem

--- rest/src/main/java/pt/ist/rest/domain/Cliente.java
+++ rest/src/main/java/pt/ist/rest/domain/Cliente.java
@@ -57,11 +57,7 @@ public class Cliente extends Cliente_Base {
 
     public void confirmaCompra(){
 
-<<<<<<< HEAD
-        final int CUSTO = compra.getCusto();
-||||||| merged common ancestors
-        final int CUSTO = compra.somaCusto();
-=======
+
 		 if (!hasComprasAberta())
             System.out.println("EXCEPCAO O TABULEIRO DE COMPRAS ESTA VAZIO");     
 		else 
@@ -72,7 +68,7 @@ public class Cliente extends Cliente_Base {
 			 System.out.println("EXCEPCAO O TABULEIRO DE COMPRAS ESTA VAZIO");     
 			 
         final int CUSTO = compra.getCusto();
->>>>>>> d15a70b4d86b2d3ad6c2015b2b1323567a7f8d4f
+
         final int SALDO = getSaldo();
         if (SALDO > CUSTO){
             this.setSaldo(SALDO - CUSTO);
