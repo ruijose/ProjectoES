e8dbe351369f453d565ed1afa6ee9b068fbef346
ruimangas <ruijosemangas@gmail.com>
2014-03-18 13:58:13 +0000

    servico e merges

--- rest/src/main/java/pt/ist/rest/domain/Cliente.java
+++ rest/src/main/java/pt/ist/rest/domain/Cliente.java
@@ -57,35 +57,20 @@ public class Cliente extends Cliente_Base {
 
     public void confirmaCompra(){
 
-<<<<<<< HEAD
 
-		 if (!hasComprasAberta())
-||||||| merged common ancestors
-<<<<<<< HEAD
-        final int CUSTO = compra.getCusto();
-||||||| merged common ancestors
-        final int CUSTO = compra.somaCusto();
-=======
-		 if (!hasComprasAberta())
-=======
+
 		if (!hasComprasAberta())
->>>>>>> ed4a50e111ca9a4a56e75186d5d5cd795863d77e
             System.out.println("EXCEPCAO O TABULEIRO DE COMPRAS ESTA VAZIO");     
 		
 		final Compra compra = this.getCompraAberta();
-		
 		final boolean compraTemItems = (compra.getItemCount() > 0);
+
 		if (!compraTemItems)
 			 System.out.println("EXCEPCAO O TABULEIRO DE COMPRAS ESTA VAZIO");     
 			 
         final int CUSTO = compra.getCusto();
-<<<<<<< HEAD
-
-||||||| merged common ancestors
->>>>>>> d15a70b4d86b2d3ad6c2015b2b1323567a7f8d4f
-=======
->>>>>>> ed4a50e111ca9a4a56e75186d5d5cd795863d77e
         final int SALDO = getSaldo();
+        
         if (SALDO > CUSTO){
             this.setSaldo(SALDO - CUSTO);
             compra.setConfirma(new Integer(1));
